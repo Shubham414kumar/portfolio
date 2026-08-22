851dd563ec0ce0a4cb030fa6251dcdb2165a23e3
Shubham Kumar <shubhankumarkir@gmail.com>
2026-08-22 16:46:59 +0800

    Configure automatic deployment to GitHub Pages

--- src/router.tsx
+++ src/router.tsx
@@ -8,6 +8,7 @@ export const getRouter = () => {
   const router = createRouter({
     routeTree,
     context: { queryClient },
+    basepath: "/portfolio/",
     scrollRestoration: true,
     defaultPreloadStaleTime: 0,
   });
